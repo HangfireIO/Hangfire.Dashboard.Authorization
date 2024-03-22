53e4e7d3cfe657f0385d00e9b37e9f6148ebcbfa
Sergey Odinokov <odinserj@hangfire.io>
2024-03-22 16:20:15 +0700

    Add assembly and NuGet package signing

--- psake-project.ps1
+++ psake-project.ps1
@@ -2,7 +2,7 @@ Include "packages\Hangfire.Build.0.4.3\tools\psake-common.ps1"
 
 Task Default -Depends Pack
 
-Task Merge -Depends Compile -Description "Run ILMerge /internalize to merge assemblies." {
+Task Merge -Depends Compile -Description "Run ILRepack /internalize to merge assemblies." {
     Repack-Assembly @("Hangfire.Dashboard.Authorization", "net45") @("Microsoft.Owin")
 }
 
@@ -16,4 +16,9 @@ Task Pack -Depends Collect -Description "Create NuGet packages and archive files
     
     Create-Package "Hangfire.Dashboard.Authorization" $version
     Create-Archive "Hangfire.Dashboard.Authorization-$version"
+}
+
+Task Sign -Depends Pack -Description "Sign artifacts." {
+    $version = Get-PackageVersion
+    Sign-ArchiveContents "Hangfire.Dashboard.Authorization-$version" "hangfire"
 }
